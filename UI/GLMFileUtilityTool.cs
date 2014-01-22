5e04cc775a86f74c5412191a1bc9dd447bae0468
Shannon Stubsten <shannon.stubsten@foundant.com>
2014-01-22 09:40:25 -0700

    re-referenced custom control

--- UI/GLMFileUtilityTool.cs
+++ UI/GLMFileUtilityTool.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using UI.Controls;
 using UI.Controls;
 using UI.Controls.FunctionBlockControls;
 
