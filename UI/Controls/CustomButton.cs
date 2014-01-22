dceeaee75108a7332c6d5b3e6eab6c8e582e36fc
Shannon Stubsten <shannon.stubsten@foundant.com>
2014-01-22 09:02:35 -0700

    Added new function panels, modified main form navigation

---
+++ UI/Controls/CustomButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace UI.Controls
+{
+	public class CustomButton : Button
+	{
+		public CustomButton()
+		{
+			FlatAppearance.BorderSize = 0;
+			FlatStyle = FlatStyle.Flat;
+		}
+
+		protected override void OnPaint(PaintEventArgs pevent)
+		{
+			base.OnPaint(pevent);
+
+			Pen pen = new Pen(FlatAppearance.BorderColor, 1);
+			Rectangle rectangle = new Rectangle(0,0, Size.Width -1, Size.Height -1);
+			pevent.Graphics.DrawRectangle(pen, rectangle);
+		}
+
+		protected override bool ShowFocusCues
+		{
+			get { return false; }
+		}
+	}
+}
