3aa605a105459eb4fe8fa1ec90c1eeb319f344b7
Joey Dennehy <joey.dennehy@foundant.com>
2014-01-23 10:24:12 -0700

    File Itteration count accepted

--- API/ApplicantProcessQuery.cs
+++ API/ApplicantProcessQuery.cs
@@ -8,40 +8,38 @@ using System.Linq;
 
 namespace API
 {
-	 public class ApplicantProcessQuery
-	 {
-		
+	public class ApplicantProcessQuery
+	{
+
 		//public  methods to retrieve:
 
 		//1Client URLKeys and names
-		 public Dictionary<string, string> RetrieveFoundationInformation()
+		public Dictionary<string, string> RetrieveFoundationInformation()
 		{
-			Command command = new Command
-			{
-				SqlStatementId = "SELECT_ALL_URL_KEYS_AND_NAMES"
-			};
+			Command command = new Command {SqlStatementId = "SELECT_ALL_URL_KEYS_AND_NAMES"};
 
 			DataAccess access = new DataAccess();
 
 			Dictionary<string, string> foundations = new Dictionary<string, string>();
 
-			 using (MySqlDataReader reader = access.GetReader(command))
-			 {
-				 while (reader.Read())
-				 {
-					 if (!reader.IsDBNull(0))
-					 {
-						 var foundationId = reader.GetString(0);
-						 foundations.Add(foundationId + " - " + reader.GetString(1), foundationId);
-					 }
-				 }
-			 }
+			using (MySqlDataReader reader = access.GetReader(command))
+			{
+				while (reader.Read())
+				{
+					if (!reader.IsDBNull(0))
+					{
+						var foundationId = reader.GetString(0);
+						foundations.Add(foundationId + " - " + reader.GetString(1), foundationId);
+					}
+				}
+			}
 
 
-			 return foundations;
+			return foundations;
 		}
+
 		// 2All process IDs
-		 public Dictionary<string, string> RetrieveFoundationProcessInfo(string urlKey)
+		public Dictionary<string, string> RetrieveFoundationProcessInfo(string urlKey)
 		{
 			ParameterSet parameters = new ParameterSet();
 			parameters.Add(DbType.String, "URL_KEY", urlKey);
@@ -92,50 +90,55 @@ namespace API
 			{
 				Directory.CreateDirectory(copyDirectory);
 			}
+			List<string> files = new List<string>();
 			foreach (var applicantProcessId in applicantProcessIds)
 			{
 				string directoryPath = baseDirectory + "/" + urlKey + "/" + applicantProcessId;
 				if (Directory.Exists(directoryPath))
 				{
-					string[] files = Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories);
-					bool hasOtherStages = files.Any(file => Path.GetDirectoryName(file)
-					                                                 .TrimEnd(Path.DirectorySeparatorChar)
-					                                                 .Split(Path.DirectorySeparatorChar)
-					                                                 .Last()
-					                                                 .Contains("loi") || Path.GetDirectoryName(file)
-					                                                                         .TrimEnd(Path.DirectorySeparatorChar)
-					                                                                         .Split(Path.DirectorySeparatorChar)
-					                                                                         .Last()
-					                                                                         .Contains("qualification"));
-					// Copy the files and overwrite destination files if they already exist. 
-					foreach (string file in files)
-					{
-						// Use static Path methods to extract only the file name from the path.
-						string stage = Path.GetDirectoryName(file)
-						                      .TrimEnd(Path.DirectorySeparatorChar)
-						                      .Split(Path.DirectorySeparatorChar)
-						                      .Last();
-						string fileName = string.Format("{0}{1}_{2}",applicantProcessId, hasOtherStages ? "_" + stage : "", Path.GetFileName(file));
-						string destFile = Path.Combine(copyDirectory, fileName);
-						
-						if (File.Exists(destFile))
-						{
-							int interation = 1;
-							while (!File.Exists(destFile))
-							{
-								destFile = destFile + "(" + interation + ")";
-								++interation;
-							}
-						}
-						File.Copy(file, destFile, true);
-					}
+					files.AddRange(Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories));
 				}
-				else
+			}
+
+
+			bool hasOtherStages = files.Any(file => Path.GetDirectoryName(file)
+			                                            .TrimEnd(Path.DirectorySeparatorChar)
+			                                            .Split(Path.DirectorySeparatorChar)
+			                                            .Last()
+			                                            .Contains("loi") || Path.GetDirectoryName(file)
+			                                                                    .TrimEnd(Path.DirectorySeparatorChar)
+			                                                                    .Split(Path.DirectorySeparatorChar)
+			                                                                    .Last()
+			                                                                    .Contains("qualification"));
+			// Copy the files and overwrite destination files if they already exist. 
+			foreach (string file in files)
+			{
+				// Use static Path methods to extract only the file name from the path.
+				string[] directory = Path.GetDirectoryName(file)
+				                         .TrimEnd(Path.DirectorySeparatorChar)
+				                         .Split(Path.DirectorySeparatorChar);
+				string stage = directory.Last();
+				string applicantProcessId = directory[directory.Count() - 2];
+				string fileName = string.Format("{0}{1}_{2}", applicantProcessId, hasOtherStages ? "_" + stage : "",
+				                                Path.GetFileName(file));
+				string destFile = Path.Combine(copyDirectory, fileName);
+
+				if (File.Exists(destFile))
 				{
-					Console.WriteLine("Source path does not exist!");
+					int interation = 1;
+					string tempFullFileName = destFile;
+					while (!File.Exists(tempFullFileName))
+					{
+						string tempFileName = Path.GetFileNameWithoutExtension(fileName) + "(" + interation + ")" + Path.GetExtension(fileName);
+						tempFullFileName = Path.Combine(copyDirectory, tempFileName);
+						++interation;
+					}
+					destFile = tempFullFileName;
 				}
+				File.Copy(file, destFile, true);
 			}
 		}
 
-	 }
+	}
+
 }
