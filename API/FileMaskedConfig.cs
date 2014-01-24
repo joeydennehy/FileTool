5a2e8688aa9c0152af0692e27f4e445acbbff8a9
Joey Dennehy <joey.dennehy@foundant.com>
2014-01-24 13:43:18 -0700

    added filemask

---
+++ API/FileMaskedConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace API
+{
+	class FileMaskedConfig : ConfigurationSection
+	{
+		private static readonly Hashtable settings = (Hashtable)ConfigurationManager.GetSection("FileMaskedConfig");
+
+		public static Dictionary<string,string> Settings
+		{
+			get
+			{
+				return settings.Cast<DictionaryEntry>().ToDictionary(d => (string)d.Key, d => (string)d.Value);
+			}
+		}
+	}
+}
