bf0bac4ad616a014bcc5a16cc8582abe72087bfa
Shannon Stubsten <shannon.stubsten@foundant.com>
2014-02-12 11:32:50 -0700

    modifying event handle to retrieve selected item instead of selected value

--- UI/Controls/FunctionBlockControls/ClientFileExtractionControl.cs
+++ UI/Controls/FunctionBlockControls/ClientFileExtractionControl.cs
@@ -12,7 +12,6 @@ namespace UI.Controls.FunctionBlockControls
 {
 	public partial class ClientFileExtractionControl : FunctionBlockBaseControl
 	{
-
 		#region Member Variables
 
 		private const string APPLICANT_PROCESS_FORMAT = "Total Applicant Process IDs: {0}";
@@ -57,9 +56,12 @@ namespace UI.Controls.FunctionBlockControls
 			try
 			{
 				RequestQuery.RefreshFoundationProcessData(state.FoundationId);
-				
+
 				if (RequestQuery.FoundationProcessData.Rows.Count == 0)
+				{
+					processIdComboBox.DataSource = null;
 					return;
+				}
 
 				processIdComboBox.DataSource = RequestQuery.FoundationProcessData;
 				processIdComboBox.DisplayMember = "ProcessDisplayText";
@@ -68,6 +70,21 @@ namespace UI.Controls.FunctionBlockControls
 			catch (Exception eError)
 			{
 				MessageBox.Show(this, string.Format(FILE_COPY_ERROR_FORMAT, eError.Message), FILE_COPY_CAPTION, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		
+		private void HandleFoundationSelectionChanged(DataRow selectedRow)
+		{
+			var selectedFoundationId = (int)selectedRow[0];
+			var selectedUrlKey = (string)selectedRow[1];
+
+			if (state.FoundationId != selectedFoundationId)
+			{
+				state.FoundationId = selectedFoundationId;
+				state.FoundationUrlKey = selectedUrlKey;
+
+				SetProcessingFolderText();
+				BindFoundationProcessData();
 			}
 		}
 
@@ -264,22 +281,7 @@ namespace UI.Controls.FunctionBlockControls
 
 		private void SelectedValueChanged_FoundationDropDown(object sender, EventArgs e)
 		{
-			HandleFoundationSelectionChanged(((DataRowView)foundationIdComboBox.SelectedValue).Row);
-		}
-
-		private void HandleFoundationSelectionChanged(DataRow selectedRow)
-		{
-			var selectedFoundationId = (int)selectedRow[0];
-			var selectedUrlKey = (string)selectedRow[1];
-
-			if (state.FoundationId != selectedFoundationId)
-			{
-				state.FoundationId = selectedFoundationId;
-				state.FoundationUrlKey = selectedUrlKey;
-
-				SetProcessingFolderText();
-				BindFoundationProcessData();
-			}
+			HandleFoundationSelectionChanged(((DataRowView)foundationIdComboBox.SelectedItem).Row);
 		}
 
 		private void SelectedIndexChanged_ProcessIdComboBox(object sender, EventArgs e)
