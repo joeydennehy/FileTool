3dc975ae491d6cb62617618d683ce6df5d0b3da3
Shannon Stubsten <shannon.stubsten@foundant.com>
2014-02-21 10:15:24 -0700

    updated utility meta info

--- UI/Properties/AssemblyInfo.cs
+++ UI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GLM File Utility")]
+[assembly: AssemblyDescription("GLM File Utility")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("UI")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCompany("Foundant Techonologies")]
+[assembly: AssemblyProduct("GLM File Utility")]
+[assembly: AssemblyCopyright("Copyright © Foundant Technologies 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
